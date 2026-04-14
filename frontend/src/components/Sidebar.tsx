cb58ad30b99f13209ffc01ff0cf3fb8225a3d973
Yossyaze <yossyaze0811@gmail.com>
2026-04-14 23:54:37 +0900

    style(sidebar): コマンドのコピーアイコンをクリップに変更

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -649,7 +649,7 @@ const SortableCommandItem = React.memo(
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
-                    d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                    d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
                   />
                 </svg>
               </button>
